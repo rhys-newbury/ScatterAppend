cb3803561ac922c4b334858a67111c5100652b75
taco <renew3@student.monash.edu>
2024-04-15 15:35:16 +1000

    test py3.6

--- scatter_append/scatter_append.py
+++ scatter_append/scatter_append.py
@@ -1,6 +1,5 @@
 from typing import MutableSequence, Any, Tuple
 
-
 class ScatterAppend:
     @classmethod
     def make_n(cls, n: int):
@@ -9,7 +8,7 @@ class ScatterAppend:
     def __init__(self, *refs: MutableSequence[Any]) -> None:
         self.refs = refs
 
-    def __iadd__(self, others: tuple[Any, ...]):
+    def __iadd__(self, others: Tuple[Any, ...]):
         for ref, o in zip(self.refs, others):
             ref.append(o)
         return self
@@ -20,7 +19,7 @@ class ScatterAppend:
     def __repr__(self):
         return str([x for x in self.refs])
 
-    def get(self, index: int) -> tuple[Any, ...]:
+    def get(self, index: int) -> Tuple[Any, ...]:
         return tuple(x[index] for x in self.refs)
 
     def size(self, i=None):
